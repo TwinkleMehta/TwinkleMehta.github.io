3805f1db37ee687bf99178583513fd876f78eff0
TwinkleMehta <twinklemehta8@gmail.com>
2021-12-24 15:25:35 -0500

    updating build fr

--- src/components/AllLinks.js
+++ src/components/AllLinks.js
@@ -7,7 +7,7 @@ function AllLinks() {
         <div className='link-container'>
             <a href="https://linkedin.com/in/TwinkleMehta">Linkedin</a>
             <a href="https://github.com/TwinkleMehta">Github</a>
-            <a href={process.env.PUBLIC_URL + "/Twinkle-Mehta-Resume.pdf"}>Resume</a>
+            <a href={process.env.PUBLIC_URL + "/Twinkle-Mehta-Resume-Sept21.pdf"}>Resume</a>
             <a href="https://twitter.com/Twinkle_Mehta_">Twitter</a>
             <a href="mailto:twinklemehta8@gmail.com">Email</a>
         </div>
